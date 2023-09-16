1b44722cf0fea38cd4909ef87145c27961fc21b4
AngeloBoggio <91862454+AngeloBoggio@users.noreply.github.com>
2023-09-16 01:04:05 -0400

    Created Search Bar

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,9 +1,21 @@
+import React from "react";
 import "./Home.css";
 
 export default function Home() {
-    return (
-        <div>
-            Hi
-        </div>
-    )
+  return (
+    <div className="container mt-5">
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search"
+          aria-label="Search"
+          aria-describedby="button-addon2"
+        />
+        <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+          Search
+        </button>
+      </div>
+    </div>
+  );
 }
