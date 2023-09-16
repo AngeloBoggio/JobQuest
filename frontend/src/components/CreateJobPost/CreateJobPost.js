df03f578295ed75025796fbdc992c2346c3e0469
AngeloBoggio <91862454+AngeloBoggio@users.noreply.github.com>
2023-09-16 11:17:17 -0400

    Created user tags for the job posting specific technologies

--- frontend/src/components/CreateJobPost/CreateJobPost.js
+++ frontend/src/components/CreateJobPost/CreateJobPost.js
@@ -4,26 +4,24 @@ import Button from "react-bootstrap/esm/Button";
 import Modal from "react-bootstrap/Modal";
 import { firestore } from "../../firebase";
 import {addDoc,collection} from "firebase/firestore"
-import {useSelector} from "react-redux";
-import {selectUserId} from "../../store/userCredentials/userCredentialsSelectors";
 
 export default function CreateJobPost() {
   const [showCreatePost, setShowCreatePost] = useState(false);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [tags, setTags] = useState("");
+  const [tags, setTags] = useState([]);
+  const [currentTag, setCurrentTag] = useState("");
 
-  const ref = collection(firestore, "jobPosts");
-    const userId = useSelector((state) => selectUserId(state));
+  const ref = collection(firestore, "jobpost");
+
 
   const createPost = async (event) => {
     event.preventDefault();
     try{
-        await addDoc(ref, {title: title, tags: tags, description: description, userId: userId})
+        await addDoc(ref, {title: title, tags: tags, description: description})
     }catch(e){
         console.log(e);
     }
-
   };
 
   return (
@@ -47,53 +45,66 @@ export default function CreateJobPost() {
           </Modal.Header>
           <Modal.Body>
             <div className="mb-3">
-                <form id="job-post-form">
+              <form id="job-post-form">
                 <div className="mb-3">
-                    <label htmlFor="job-title" className="form-label">Job Title</label>
-                    <input 
+                  <label htmlFor="job-title" className="form-label">Job Title</label>
+                  <input 
                     type="text" 
                     className="form-control" 
                     id="job-title" 
                     required  
                     value={title}
                     onChange={(event) => setTitle(event.target.value)}
-                    />
-                    
+                  />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="job-description" className="form-label">Job Description</label>
-                    <textarea 
+                  <label htmlFor="job-description" className="form-label">Job Description</label>
+                  <textarea 
                     className="form-control" 
                     id="job-description" 
                     rows="3" 
                     required 
                     value={description}
                     onChange={(event) => setDescription(event.target.value)}
-                    />
+                  />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="job-tags" className="form-label">Technology Tags</label>
-                    <input 
+                  <label htmlFor="job-tags" className="form-label">Technology Tags</label>
+                  <input 
                     type="text" 
                     className="form-control" 
                     id="job-tags" 
                     placeholder="e.g., JavaScript, Python" 
-                    required 
-                    value={tags}
-                    onChange={(event) => setTags(event.target.value)}
-                    />
+                    value={currentTag}
+                    onChange={(event) => setCurrentTag(event.target.value)}
+                    onKeyDown={(event) => {
+                      if (event.key === "Enter") {
+                        event.preventDefault();
+                        addTag();
+                      }
+                    }}
+                    onBlur={addTag}
+                  />
+                  <div className="tags-container">
+                    {tags.map((tag, index) => (
+                      <div key={index} className="tag">
+                        {tag}
+                        <span className="close-icon" onClick={() => removeTag(tag)}>X</span>
+                      </div>
+                    ))}
+                  </div>
                 </div>
-                </form>
+              </form>
             </div>
-            </Modal.Body>
-            <Modal.Footer>
+          </Modal.Body>
+          <Modal.Footer>
             <Button variant="secondary" onClick={() => setShowCreatePost(false)}>
-                Close
+              Close
             </Button>
-            <Button variant="primary" type="submit" onClick={ createPost}>
-                Post
+            <Button variant="primary" type="submit" onClick={createPost}>
+              Post
             </Button>
-            </Modal.Footer>
+          </Modal.Footer>
         </Modal>
       </div>
     </div>
