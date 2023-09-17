7b7a9b04a39d5eb50ded3fe332213ad13c0b98d7
Javier Daniel Garcia <javierdangarcia@gmail.com>
2023-09-16 22:41:48 -0400

    fixed job details design

--- frontend/src/components/JobDetails/JobDetails.js
+++ frontend/src/components/JobDetails/JobDetails.js
@@ -1,11 +1,12 @@
+import "./JobDetails.css";
 import Card from "react-bootstrap/Card";
 import { useParams } from "react-router-dom";
-import {useEffect, useState} from "react";
-import {collection, onSnapshot, query, where} from "firebase/firestore";
-import {firestore} from "../../firebase";
-import {collections} from "../../enums/collections";
-import {useSelector} from "react-redux";
-import {selectUserId} from "../../store/userCredentials/userCredentialsSelectors";
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { firestore } from "../../firebase";
+import { collections } from "../../enums/collections";
+import { useSelector } from "react-redux";
+import { selectUserId } from "../../store/userCredentials/userCredentialsSelectors";
 
 export default function JobDetails() {
     const collectionRef = collection(firestore, collections.jobPostings);
@@ -35,19 +36,93 @@ export default function JobDetails() {
         };
     }, []);
 
+    function formatDate(timestamp) {
+        const date = new Date(timestamp * 1000); // Convert seconds to milliseconds
+
+        // Get the day of the month
+        const day = date.getDate();
+
+        // Function to determine the day suffix
+        function getDaySuffix(day) {
+            if (day >= 11 && day <= 13) {
+                return "th";
+            }
+            switch (day % 10) {
+                case 1:
+                    return "st";
+                case 2:
+                    return "nd";
+                case 3:
+                    return "rd";
+                default:
+                    return "th";
+            }
+        }
+
+        // Format the date
+        const options = { month: "long" };
+        const formattedDate = date.toLocaleDateString(undefined, options);
+        const daySuffix = getDaySuffix(day);
+        return `${formattedDate} ${day}${daySuffix}`;
+    }
+
     return (
-        <div className="d-flex justify-content-center align-items-center mt-5">
-            {job ? <Card style={{width: "40rem"}}>
-                <Card.Body>
-                    <Card.Title>{job.data.title}</Card.Title>
-                    <Card.Text className="mb-2">{job.data.companyName}</Card.Text>
-                    <Card.Text className="mb-2">
-                        job.data.description
-                    </Card.Text>
-                    <Card.Text>{job.data.tags}</Card.Text>
-                </Card.Body>
-                <Card.Footer className="text-muted">2 days ago</Card.Footer>
-            </Card> : <div></div>}
-        </div>
+        job ? (
+            <div className="d-flex justify-content-center mt-4 mb-4">
+                <Card style={{ width: "1200px" }}>
+                    <div className="d-flex">
+                        <div className="line-container">
+                            <img
+                                src="https://expresswriters.com/wp-content/uploads/2015/09/google-new-logo-450x450.jpg"
+                                style={{ width: "200px", height: "200px" }}
+                            />
+                        </div>
+                        <Card.Body className="ps-0">
+                            <Card.Title>
+                                <div className="d-flex justify-content-between">
+                                    <p className="m-0">
+                                        {job.data.title} (
+                                        {job.data.companyName})
+                                    </p>
+                                    <p className="m-0">
+                                        Salary: {Number(job.data.salary).toLocaleString("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 })}
+                                    </p>
+                                </div>
+                            </Card.Title>
+                            <Card.Text className="mb-2">
+                                Location: {job.data.location}
+                            </Card.Text>
+                            <Card.Text className="mb-2">
+                                Description: {job.data.description}
+                            </Card.Text>
+                            <div className="d-flex">
+                                {job.data.tags.map((tag) => (
+                                    <Card.Text className="m-0 me-2">
+                                        <p className="job-tag">{tag}</p>
+                                    </Card.Text>
+                                ))}
+                            </div>
+                            <p className="mt-3 mb-3 m-0">Learn these skills to land the job!</p>
+                            <div className="row">
+                                {job.data.videos.map((video, index) => (
+                                    <div key={index} className="col-md-6 mb-3">
+                                        <iframe
+                                            className="iframe-border"
+                                            width="100%"
+                                            height="250"
+                                            src={video}
+                                            title={`YouTube Video ${index + 1}`}
+                                            allowFullScreen
+                                        ></iframe>
+                                    </div>
+                                ))}
+                            </div>
+                        </Card.Body>
+                    </div>
+                    <Card.Footer>
+                        Posted on {formatDate(job.data.createdDate)}
+                    </Card.Footer>
+                </Card >
+            </div >) : null
     );
 }
